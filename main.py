5e07fab4dc33a8eb7ba6dfe42c5fa6958248cf1c
MarianoNLR <marianonehuenlr@gmail.com>
2025-04-03 08:34:12 -0300

    + funcionalidad botones sidebar opcion 2-4

--- main.py
+++ main.py
@@ -82,7 +82,99 @@ def show_option1_frame():
     # Botón para procesar archivos
     process_files_button = tk.Button(opcion1_frame, text="Procesar archivos", command=on_process_files_button_click, relief="flat", bg="#ffffff", fg="#000000", height=3, font=("Arial", 12))
     process_files_button.grid(row=2, columnspan=2)
-    pass
+    
+### Mostrar Frame de Opcion 2
+def show_option2_frame():
+    for frame in main_content.winfo_children():
+        frame.destroy()
+    
+    # Creacion de frames
+    opcion2_frame =tk.Frame(main_content, bg="#212121", height=300)
+    opcion2_frame.grid(row=0, column=0, sticky="nsew")
+    
+    # Configuracion de grid opcion 2
+    opcion2_frame.grid_rowconfigure(0, weight=1)
+    opcion2_frame.grid_rowconfigure(1, weight=1)
+    opcion2_frame.grid_rowconfigure(2, weight=3)
+    opcion2_frame.grid_columnconfigure(0, weight=1)
+    opcion2_frame.grid_columnconfigure(1, weight=1)
+    
+    ### Contenido principal frame opcion 2
+    # # Botones de seleccion de archivos
+    # open_excel_button = tk.Button(opcion1_frame ,text="Seleccionar Excel", command=on_open_excel_button_click, relief="flat", bg="#eb2b2b", fg="white", height=3, font=("Arial", 12))
+    # open_txt_button = tk.Button(opcion1_frame, text="Seleccionar Txt", command=on_open_txt_button_click, relief="flat", bg="#eb2b2b", fg="white", height=3, font=("Arial", 12))
+    # open_excel_button.grid(row=1, column=0)
+    # open_txt_button.grid(row=1, column=1)
+    
+    # Titulo del frame
+    label = tk.Label(opcion2_frame, text="Bienvenido a la Opcion 2", wraplength=350, font=("Arial", 40), bg="#212121", fg="white")
+    label.grid(row=0, column=0, columnspan=2, sticky="nsew")
+    
+    # Botón para procesar archivos
+    # process_files_button = tk.Button(opcion1_frame, text="Procesar archivos", command=on_process_files_button_click, relief="flat", bg="#ffffff", fg="#000000", height=3, font=("Arial", 12))
+    # process_files_button.grid(row=2, columnspan=2)
+    
+### Mostrar Frame de Opcion 3
+def show_option3_frame():
+    for frame in main_content.winfo_children():
+        frame.destroy()
+    
+    # Creacion de frames
+    opcion3_frame =tk.Frame(main_content, bg="#212121", height=300)
+    opcion3_frame.grid(row=0, column=0, sticky="nsew")
+    
+    # Configuracion de grid opcion 3
+    opcion3_frame.grid_rowconfigure(0, weight=1)
+    opcion3_frame.grid_rowconfigure(1, weight=1)
+    opcion3_frame.grid_rowconfigure(2, weight=3)
+    opcion3_frame.grid_columnconfigure(0, weight=1)
+    opcion3_frame.grid_columnconfigure(1, weight=1)
+    
+    ### Contenido principal frame opcion 3
+    # # Botones de seleccion de archivos
+    # open_excel_button = tk.Button(opcion1_frame ,text="Seleccionar Excel", command=on_open_excel_button_click, relief="flat", bg="#eb2b2b", fg="white", height=3, font=("Arial", 12))
+    # open_txt_button = tk.Button(opcion1_frame, text="Seleccionar Txt", command=on_open_txt_button_click, relief="flat", bg="#eb2b2b", fg="white", height=3, font=("Arial", 12))
+    # open_excel_button.grid(row=1, column=0)
+    # open_txt_button.grid(row=1, column=1)
+    
+    # Titulo del frame
+    label = tk.Label(opcion3_frame, text="Bienvenido a la Opcion 3", wraplength=350, font=("Arial", 40), bg="#212121", fg="white")
+    label.grid(row=0, column=0, columnspan=2, sticky="nsew")
+    
+    # # Botón para procesar archivos
+    # process_files_button = tk.Button(opcion1_frame, text="Procesar archivos", command=on_process_files_button_click, relief="flat", bg="#ffffff", fg="#000000", height=3, font=("Arial", 12))
+    # process_files_button.grid(row=2, columnspan=2)
+    
+### Mostrar Frame de Opcion 4
+def show_option4_frame():
+    for frame in main_content.winfo_children():
+        frame.destroy()
+    
+    # Creacion de frames
+    opcion4_frame =tk.Frame(main_content, bg="#212121", height=300)
+    opcion4_frame.grid(row=0, column=0, sticky="nsew")
+    
+    # Configuracion de grid opcion 4
+    opcion4_frame.grid_rowconfigure(0, weight=1)
+    opcion4_frame.grid_rowconfigure(1, weight=1)
+    opcion4_frame.grid_rowconfigure(2, weight=3)
+    opcion4_frame.grid_columnconfigure(0, weight=1)
+    opcion4_frame.grid_columnconfigure(1, weight=1)
+    
+    ### Contenido principal frame opcion 4
+    # # Botones de seleccion de archivos
+    # open_excel_button = tk.Button(opcion1_frame ,text="Seleccionar Excel", command=on_open_excel_button_click, relief="flat", bg="#eb2b2b", fg="white", height=3, font=("Arial", 12))
+    # open_txt_button = tk.Button(opcion1_frame, text="Seleccionar Txt", command=on_open_txt_button_click, relief="flat", bg="#eb2b2b", fg="white", height=3, font=("Arial", 12))
+    # open_excel_button.grid(row=1, column=0)
+    # open_txt_button.grid(row=1, column=1)
+    
+    # Titulo del frame
+    label = tk.Label(opcion4_frame, text="Bienvenido a la Opcion 4", wraplength=350, font=("Arial", 40), bg="#212121", fg="white")
+    label.grid(row=0, column=0, columnspan=2, sticky="nsew")
+    
+    # # Botón para procesar archivos
+    # process_files_button = tk.Button(opcion1_frame, text="Procesar archivos", command=on_process_files_button_click, relief="flat", bg="#ffffff", fg="#000000", height=3, font=("Arial", 12))
+    # process_files_button.grid(row=2, columnspan=2)
 
 if __name__ == "__main__":
     ### Config ventana principal
@@ -111,9 +203,9 @@ if __name__ == "__main__":
     
     ### Barra lateral izquierda opciones
     sidebar_opt1 = tk.Button(sidebar, text="Opcion 1", relief="flat", height=2, foreground="white", bg="#eb2b2b", font=("Arial", 12), command=show_option1_frame)
-    sidebar_opt2 = tk.Button(sidebar, text="Opcion 2", relief="flat", height=2, foreground="white", bg="#eb2b2b", font=("Arial", 12))
-    sidebar_opt3 = tk.Button(sidebar, text="Opcion 3", relief="flat", height=2, foreground="white", bg="#eb2b2b", font=("Arial", 12))
-    sidebar_opt4 = tk.Button(sidebar, text="Opcion 4", relief="flat", height=2, foreground="white", bg="#eb2b2b", font=("Arial", 12))
+    sidebar_opt2 = tk.Button(sidebar, text="Opcion 2", relief="flat", height=2, foreground="white", bg="#eb2b2b", font=("Arial", 12), command=show_option2_frame)
+    sidebar_opt3 = tk.Button(sidebar, text="Opcion 3", relief="flat", height=2, foreground="white", bg="#eb2b2b", font=("Arial", 12), command=show_option3_frame)
+    sidebar_opt4 = tk.Button(sidebar, text="Opcion 4", relief="flat", height=2, foreground="white", bg="#eb2b2b", font=("Arial", 12), command=show_option4_frame)
     
     sidebar_opt1.pack(fill="x", pady=0)
     sidebar_opt2.pack(fill="x", pady=1)
