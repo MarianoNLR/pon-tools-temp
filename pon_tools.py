508a3a8a8b9a3a7d7e19c9618b5e4d43624aeadd
MarianoNLR <marianonehuenlr@gmail.com>
2025-04-02 23:24:40 -0300

    refactor

--- pon_tools.py
+++ pon_tools.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import openpyxl
 
-def create_excel(df):
+def create_excel(df, data):
     #Crear libro de excel
     wb = openpyxl.Workbook()
     sheet = wb.active
@@ -13,26 +13,10 @@ def create_excel(df):
     sheet.append(tuple(df.columns))
 
     # Agregar coincidencias al nuevo excel
+    coincidences = df[df['DNI'].isin(data)]
     for item in coincidences.itertuples(index=False):
         if item.index == 0:
             continue
         sheet.append(tuple(item))
-    
-    return wb
-
-if __name__ == "__main__":
-    # Leer excel y cargar en data frame
-    df = pd.read_excel("./example.xlsx")
-    df["DNI"] = df["DNI"].astype(str)
-    
-    #Leer archivo de texto
-    with open("./example_txt.txt", "r") as txt:
-        data = []
-        # Leer archivo de texto linea por linea
-        for line in txt:
-            data.append(line.strip())
-
-    # Obtener las coincidencias entre excel y txt por DNI
-    coincidences = df[df['DNI'].isin(data)]
-    wb = create_excel(df)
-    wb.save('./result.xlsx')
+    wb.save("./result.xlsx")
+    #return wb    
